0adc27197f0b99663c36be43c1dbff901e06075f
erselekmen <erselekmen27@gmail.com>
2025-07-09 03:33:20 +0300

    outputs cleared

--- modules/terraform-aws-vpc/outputs.tf
+++ modules/terraform-aws-vpc/outputs.tf
@@ -19,28 +19,7 @@ output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-output "public_subnet_arns" {
-  description = "List of public subnet ARNs"
-  value = aws_subnet.public[*].arn
-}
-
 output "public_subnet_cidr_blocks" {
   description = "List of public subnet CIDR blocks"
   value = aws_subnet.public[*].cidr_block
-}
-
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
-  value = concat(
-    aws_internet_gateway.this.*.id,
-    [""],
-  )[0]
-}
-
-output "public_route_table_id" {
-  description = "The ID of the public route table"
-  value = concat(
-    aws_route_table.public.*.id,
-    [""],
-  )[0]
 }
